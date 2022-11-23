113ea5d38fe0bde7a4e07b7154a5b5a65511e71a
roshanbvadassery <roshanbvadassry@gmail.com>
2022-11-23 07:52:16 +0530

    Car is here

---
+++ car.js
@@ -0,0 +1,23 @@
+class Car{
+
+    constructor(x,y,width,height){
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height; 
+
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(
+           this.x-this.width/2,
+           this.y-this.width/2,
+           this.width,
+           this.height 
+        );
+        ctx.fill();
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+myCanvas.height=window.innerHeight;
+myCanvas.width=200;
+
+const ctx=myCanvas.getContext("2d");
+const car=new Car(100, 100, 30, 50);
+car.draw(ctx);
